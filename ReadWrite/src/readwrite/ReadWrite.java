47b6811ab82bced47688d25e03548b1bd4750262
aminulhoquenusu <nusunusu111@gmail.com>
2024-05-27 18:53:59 +0600

    27/05/24

---
+++ ReadWrite/src/readwrite/ReadWrite.java
@@ -0,0 +1,25 @@
+
+package readwrite;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class ReadWrite {
+
+   
+    public static void main(String[] args) throws FileNotFoundException {
+       
+        File file= new File("C:\\Users\\Administrator\\Desktop\\Aminul.txt");
+       PrintWriter pw=new PrintWriter(file);
+       pw.write("hello");
+       pw.close();
+        
+       
+        Scanner scanner=new Scanner(file);
+        System.out.println(scanner.nextLine());
+    }
+    
+}
